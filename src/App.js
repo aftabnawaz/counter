d0d0d41070559b14b2879e66b1f13b6899ffeaaf
Aftab <aftab.nwz@gmail.com>
2021-05-06 18:22:23 +0200

    incrementButton CSS page center

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Button from './App.css';
+import divButton from './App.css';
+
 function App() {
  const [counter, setCounter] = useState(0);
  const handleClick = () => setCounter(counter+1);
   return (
-    <div>
+    <div className="divButton">
       <button className = "Button" onClick={handleClick}>
         {counter}
       </button>
