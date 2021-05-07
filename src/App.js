4c663fc3a2ea559ebeead8c52585cbe4248ee602
Aftab <aftab.nwz@gmail.com>
2021-05-07 10:03:42 +0200

    incrementButton Final Code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { useState } from 'react';
-//import counterButton from './App.css';
+import counterButton from './App.css';
 import divButton from './App.css';
-//import divDisplay from './App.css';
-
+import divDisplay from './App.css';
 function Counter(props) {
- 
-  const handleClick = () => {};
-   return (
+  const handleClick = () => props.onclickFunction(props.increment);
+      return (
      <div className="divButton">
-       <button className = "counterButton" onClick={props.onclickFunction}>
-         +1
+       <button className = "counterButton" onClick={handleClick}>
+         +{props.increment}
        </button>
        </div>
    );
 }
-
 function Display(props) {
   return (
     <div className="divDisplay">
@@ -22,18 +19,17 @@ function Display(props) {
     </div>
   )
 }
-
-function App() {
-  const [counter, setCounter] = useState(42);
-  const incrementCounter = () => setCounter(counter+1);
+function App(props) {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
   return (
     <div>
-      <Counter onclickFunction={incrementCounter} />
+      <Counter onclickFunction={incrementCounter} increment={5} />
+      <Counter onclickFunction={incrementCounter} increment={10} />
+      <Counter onclickFunction={incrementCounter} increment={15} />
+      <Counter onclickFunction={incrementCounter} increment={100} />
       <Display message={counter} />
     </div>
   )
- 
 }
-
-
 export default App;
